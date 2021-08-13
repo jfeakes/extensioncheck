00ceff9a7f1fe225e11112f0377b7b390eaf1b1d
jfeakes <71144208+jfeakes@users.noreply.github.com>
2021-08-13 13:31:44 +0800

    Update extensioncheck.py
    cleaned empty lines and added comments

--- extensioncheck.py
+++ extensioncheck.py
@@ -5,16 +5,15 @@ from posix import listdir
 windows_dir=input("Enter the directory or drag-and-drop the folder you wish to search: ")
 #Replace backslashes with forward slashes
 unix_dir= windows_dir.replace("\\","/")
-#Make drive lowercase and remove ':'
+#Make drive letter lowercase and remove colon
 unix_lower = unix_dir[0].lower() + unix_dir[0 + 2:]
 
-#Add /mnt/ for WSL
+#Add /mnt/ for WSL compatibility
 wsl_dir= ("/mnt/" + unix_lower)
 
 print("\n")
 print("Your path is: ", wsl_dir)
 print("\n")
-
 
 extension = []
 pres_ext = [".mxf", ".xml"]
@@ -27,28 +26,26 @@ while True:
     "1. PRES \n"
     "2. ACCESS\n"
     )
+    #There's definitely a better way to do this...
     if user_choice=="PRES" or user_choice== "pres" or user_choice== "1" or user_choice== "ACCESS" or user_choice== "access" or user_choice== "2":
-        
         break
     else:
         print("Invalid entry")
         continue
 
+#... and this
 if user_choice == "PRES" or user_choice == "pres" or user_choice == "1":
     extension = pres_ext
 elif user_choice == "ACCESS" or user_choice == "access" or user_choice == "2":
     extension = access_ext
         
-
 print(
     "\n"
     "Searching for files with ", extension, " extensions..."
     "\n"
     )
 
-
 results = []
-
 
 #Skips subdirectories and adds all files to the files variable
 files = (file for file in os.listdir(wsl_dir)
